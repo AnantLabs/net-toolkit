da5307d0ec32826a431f4b18c89fcc5bfc6c1a85
Christoph <Christoph@Christoph-PC>
2012-09-22 15:41:28 +0200

    Add Extension for WPF Window: Run After Loading

--- NET.Tools.WPF.Tools/Extensions/WindowExtensions.cs
+++ NET.Tools.WPF.Tools/Extensions/WindowExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Controls;
@@ -25,6 +26,24 @@ namespace NET.Tools
         public static IntPtr GetHandle(this Window window)
         {
             return new WindowInteropHelper(window).Handle;
+        }
+
+        /// <summary>
+        /// Run the action after window is completely loading
+        /// </summary>
+        /// <param name="window"></param>
+        /// <param name="action">Action to do after loading is completed (thread safe)</param>
+        public static void RunAfterLoad(this Window window, Action action)
+        {
+            new Thread(() =>
+                {
+                    while ((bool)window.Dispatcher.Invoke(new Func<bool>(() => !window.IsLoaded)))
+                    {
+                        Thread.Sleep(10);
+                    }
+
+                    window.Dispatcher.Invoke(action);
+                }).Start();
         }
     }
 }
